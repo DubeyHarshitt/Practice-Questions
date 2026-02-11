2b14dac87afd31cf20163d6bb6b3aa1c5b237def
DubeyHarshitt <harshitdubey02004@gmail.com>
2026-02-11 21:48:08 +0530

    add remove duplicates

---
+++ architechQuestions/String/capatlizeWords.js
@@ -0,0 +1,37 @@
+// 5. Capitalize words
+
+// Input: "hello world harshit"
+// Output: "Hello World Harshit"
+
+function capitalizeWords(str) {
+    let result = "";
+    let isNewWord = true;
+
+    for (let i = 0; i < str.length; i++) {
+        let ch = str[i];
+
+        // If space, next character will start a new word
+        if (ch === " ") {
+            result += ch;
+            isNewWord = true;
+        } 
+        else {
+            // Capitalize first letter of each word manually
+            if (isNewWord && ch >= 'a' && ch <= 'z') {
+                let capitalChar = String.fromCharCode(ch.charCodeAt(0) - 32);
+                result += capitalChar;
+                isNewWord = false;
+            } 
+            else {
+                result += ch;
+                isNewWord = false;
+            }
+        }
+    }
+
+    return result;
+}
+
+
+const input =  "hello world harshit";
+console.log(capitalizeWords(input));
